200c5a42a30b648869a1cb3cecba0ffdb6a03181
jeremeycharbonnet <jeremey@doctornetwork.com>
2017-06-02 15:55:54 -0500

    first commit

---
+++ android/src/main/java/com/thejump/firebaseinvites/RNFirebaseInvitesPackage.java
@@ -0,0 +1,36 @@
+package com.thejump.firebaseinvites;
+
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.thejump.firebaseinvites.RNFirebaseInvitesModule;
+
+public class RNFirebaseInvitesPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new RNFirebaseInvitesModule(reactContext));
+
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
+
